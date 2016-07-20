497ba5d716c1f9fd2d57798f80aa994eb1a1dd57
Chenyuan Xie <1147719157xie@gmail.com>
2016-07-19 22:09:47 -0400

    initial commit
     * Version : 0.1 -
     *                  Created Project

--- Assignment_4_Project_2/Program.cs
+++ Assignment_4_Project_2/Program.cs
@@ -4,6 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/* Author : Chenyuan Xie
+ * Date : July 19th, 2016
+ * Date Modified: Jul 19th, 2016
+ * Description : Assignment 4 Project 2 Airline Reservations System
+ * Version : 0.1 - 
+ *                  Created Project
+ */
 namespace Assignment_4_Project_2
 {
     class Program
