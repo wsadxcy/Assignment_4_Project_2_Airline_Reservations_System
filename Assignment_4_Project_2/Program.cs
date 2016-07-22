78873959bb8daae04ee5ecb426b4b80ddf1fe76a
Chenyuan Xie <1147719157xie@gmail.com>
2016-07-22 11:17:19 -0400

    * Version : 0.4 -  *                  Fixed the while loop

--- Assignment_4_Project_2/Program.cs
+++ Assignment_4_Project_2/Program.cs
@@ -6,10 +6,10 @@ using System.Threading.Tasks;
 
 /* Author : Chenyuan Xie
  * Date : July 19th, 2016
- * Date Modified: Jul 21th, 2016
+ * Date Modified: Jul 22th, 2016
  * Description : Assignment 4 Project 2 Airline Reservations System
- * Version : 0.3 - 
- *                  Finished part 1 and 2
+ * Version : 0.4 - 
+ *                  Fixed the while loop
  */
 namespace Assignment_4_Project_2
 {
@@ -17,38 +17,44 @@ namespace Assignment_4_Project_2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Please make a selection");
-            Console.WriteLine("1 for First Class, 2 for Economy, 3 Exit ");
-
-            int switchclass = Convert.ToInt32(Console.ReadLine());
             int firstclass = 0;
             int economy = 0;
 
-            switch(switchclass)
+            while (firstclass < 5 || economy < 5)
             {
-                case 1:
-                    Console.WriteLine("First Class");
-                    while (firstclass < 5)
-                    {
-                        firstclass++;
-                        Console.WriteLine(firstclass);
-                        break;
-                    }
-                    break;
-                case 2:
-                    Console.WriteLine("Economy");
-                    while (economy < 5)
-                    {
-                        economy++;
-                        Console.WriteLine(economy);
-                        break;
-                    }
-                    break;
-                case 3:
-                    Console.WriteLine("Exit");
-                    break;
+                Console.WriteLine("Please make a selection");
+                Console.WriteLine("1 for First Class, 2 for Economy, 3 Exit ");
 
+                int switchclass = Convert.ToInt32(Console.ReadLine());
+                
+                switch (switchclass)
+                {
+                    case 1:
+                        Console.WriteLine("First Class");
+                        while (firstclass < 5)
+                        {
+                            firstclass++;
+                            Console.WriteLine(firstclass);
+                            break;
+                        }
+                        break;
+                    case 2:
+                        Console.WriteLine("Economy");
+                        while (economy < 5)
+                        {
+                            economy++;
+                            Console.WriteLine(economy);
+                            break;
+                        }
+                        break;
+                    case 3:
+                        Console.WriteLine("Exit");
+                        break;
+
+                }
             }
+
+            
         }
     }
 }
