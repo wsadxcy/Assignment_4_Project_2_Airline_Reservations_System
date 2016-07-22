8544e633de692f445ece582deeb865b7a5c43dfe
Chenyuan Xie <1147719157xie@gmail.com>
2016-07-21 22:41:21 -0400

    * Version : 0.2 -  *                  Added the switch statement

--- Assignment_4_Project_2/Program.cs
+++ Assignment_4_Project_2/Program.cs
@@ -6,10 +6,10 @@ using System.Threading.Tasks;
 
 /* Author : Chenyuan Xie
  * Date : July 19th, 2016
- * Date Modified: Jul 19th, 2016
+ * Date Modified: Jul 21th, 2016
  * Description : Assignment 4 Project 2 Airline Reservations System
- * Version : 0.1 - 
- *                  Created Project
+ * Version : 0.2 - 
+ *                  Added the switch statement
  */
 namespace Assignment_4_Project_2
 {
@@ -17,6 +17,36 @@ namespace Assignment_4_Project_2
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Please make a selection");
+            Console.WriteLine("1 for First Class, 2 for Economy, 3 Exit ");
+
+            int switchclass = Convert.ToInt32(Console.ReadLine());
+            int firstclass = 0;
+            int economy = 0;
+
+            switch(switchclass)
+            {
+                case 1:
+                    Console.WriteLine("First Class");
+                    while (firstclass < 5)
+                    {
+                        firstclass++;
+                        Console.WriteLine(firstclass);
+                    }
+                    break;
+                case 2:
+                    Console.WriteLine("Economy");
+                    while (economy < 5)
+                    {
+                        economy++;
+                        Console.WriteLine(economy);
+                    }
+                    break;
+                case 3:
+                    Console.WriteLine("Exit");
+                    break;
+
+            }
         }
     }
 }
